94d941d482d2a794950ed3ac1b0051132a24ec26
gurjarAman <145975158+gurjarAman@users.noreply.github.com>
2024-02-27 00:40:26 +0530

    commit

---
+++ HomeDemo.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+class HomeDemo extends JPanel
+{
+JButton b1;
+JButton b2;
+HomeDemo()
+{
+b1=new JButton("Registration");
+add(b1);
+b2=new JButton("Login");
+add(b2);
+}
+}
